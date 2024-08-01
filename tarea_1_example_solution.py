c0f74df21be99f664301d5052792c4db518620fd
AGomez <agustingch@protonmail.me>
2024-07-31 22:44:55 -0400

    Se formateó el código utilizando autopep8 y se revisó con flake8. No reporta errores de formato. Propuesta de engtregrable final

--- tarea_1_example_solution.py
+++ tarea_1_example_solution.py
@@ -1,33 +1,34 @@
 def operation_selector(num1, num2, op):
     operations = ["+", "-", "*", "&"]
-    
+
     if isinstance(num1, bool) or isinstance(num2, bool):
         return -50, None
-    if not(isinstance(num1,int) and isinstance(num2,int)):
+    if not (isinstance(num1, int) and isinstance(num2, int)):
         return -50, None
     elif not isinstance(op, str):
         return -60, None
-    if isinstance(op, str) and not(op in operations):
+    if isinstance(op, str) and not (op in operations):
         return -70, None
     elif op == operations[0]:
         return 0, num1 + num2
     elif op == operations[1]:
-        return 0 , num1 - num2
+        return 0, num1 - num2
     elif op == operations[2]:
         return 0, num1 * num2
     elif op == operations[3]:
         return 0, num1 & num2
 
 
-
 def calculo_promedio(lista_valores):
-    if len(lista_valores)>10:
+    if len(lista_valores) > 10:
         return -90, None
 
-    if not all(isinstance(item, int) and not isinstance(item, bool) for item in lista_valores):
+    if not all(isinstance(item, int) and not isinstance(item, bool)
+               for item in lista_valores):
         return -80, None
-    
-    return 0, sum(lista_valores)/len(lista_valores)
-n,g = calculo_promedio([1, True, 3, 4])
-print(g)
 
+    return 0, sum(lista_valores)/len(lista_valores)
+
+
+n, g = calculo_promedio([1, True, 3, 4])
+print(g)
